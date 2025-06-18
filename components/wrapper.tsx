14cdddb74af5038279c1b780a4f3f635cccd7e4c
Ignacio Guri <ignacio.guri@holidu.com>
2025-06-18 17:56:06 +0200

    remove bg gradient

--- components/wrapper.tsx
+++ components/wrapper.tsx
@@ -52,7 +52,6 @@ export function Wrapper(props: { children: React.ReactNode }) {
 
   return (
     <div className="relative flex min-h-screen w-full justify-center bg-white bg-grid-small-black/[0.2] dark:bg-black dark:bg-grid-small-white/[0.2]">
-      <div className="pointer-events-none absolute inset-0 hidden items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black md:flex"></div>
       <div className="absolute z-50 flex w-full items-center justify-between border-b border-border bg-white px-4 py-2 dark:bg-black md:px-1 lg:w-8/12">
         {/* Header: Logo+Name, NavMenu/Burger, Theme+Account */}
         <div className="flex w-full items-center justify-between gap-2">
